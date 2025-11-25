7b04918ef5ae33ccadd1342d077bbeb712b88ffa
KevinMartinezC <ceronk003@gmail.com>
2025-11-25 16:45:39 -0600

    feat: add pagination dot

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -9,13 +9,22 @@ class MoviesSlideShow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
-        scale: 0.9,
         viewportFraction: 0.8,
+        scale: 0.9,
         autoplay: true,
+        pagination: SwiperPagination(
+          margin: EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary,
+          ),
+        ),
         itemCount: movies.length,
         itemBuilder: (context, index) {
           return _Slide(movie: movies[index]);
@@ -26,20 +35,27 @@ class MoviesSlideShow extends StatelessWidget {
 }
 
 class _Slide extends StatelessWidget {
+
   final Movie movie;
+
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
+
     final decoration = BoxDecoration(
       borderRadius: BorderRadius.circular(20),
       boxShadow: const [
-        BoxShadow(color: Colors.black45, blurRadius: 10, offset: Offset(0, 10)),
-      ],
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 10)
+        )
+      ]
     );
 
     return Padding(
-      padding: const EdgeInsets.only(bottom: 0),
+      padding: const EdgeInsets.only( bottom: 30 ),
       child: DecoratedBox(
         decoration: decoration,
         child: ClipRRect(
@@ -48,15 +64,15 @@ class _Slide extends StatelessWidget {
             movie.backdropPath,
             fit: BoxFit.cover,
             loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress != null) {
+              if ( loadingProgress != null ) {
                 return const DecoratedBox(
-                  decoration: BoxDecoration(color: Colors.black12),
+                  decoration: BoxDecoration( color: Colors.black12 )
                 );
               }
               return FadeIn(child: child);
             },
-          ),
-        ),
+          )
+        )
       ),
     );
   }
