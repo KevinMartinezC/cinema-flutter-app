b815c7b3fa5a8f2393847557de105be25961e50f
KevinMartinezC <ceronk003@gmail.com>
2025-11-26 11:38:57 -0600

    feat: display images

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinema_app/domain/models/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -22,6 +23,17 @@ class MovieHorizontalListView extends StatelessWidget {
       child: Column(
         children: [
           if (_hasTitle) _SectionHeader(title: title, subTitle: subTitle),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+               return _Slide(movie: movies[index]);
+              },
+              )
+            )
         ],
       ),
     );
@@ -30,6 +42,57 @@ class MovieHorizontalListView extends StatelessWidget {
   bool get _hasTitle => title != null || subTitle != null;
 }
 
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Image
+          SizedBox(
+            width: 150,
+            height: 225,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                height: 225,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                      ),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    width: 150,
+                    height: 225,
+                    color: Colors.grey[300],
+                    child: const Icon(Icons.broken_image),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          //Title
+        ],
+      ),
+    );
+  }
+}
 class _SectionHeader extends StatelessWidget {
   final String? title;
   final String? subTitle;
