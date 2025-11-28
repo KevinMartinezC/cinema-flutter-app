46d8bc07236f5d5b3a50c60696e57f98da8eb937
KevinMartinezC <ceronk003@gmail.com>
2025-11-28 14:16:59 -0600

    feat: add models for actors

--- lib/data/api/movie_api_client.dart
+++ lib/data/api/movie_api_client.dart
@@ -1,6 +1,6 @@
 import 'package:cinema_app/data/http/movie_db_client.dart';
 import 'package:cinema_app/data/mappers/movie_mapper.dart';
-import 'package:cinema_app/data/models/move_result.dart';
+import 'package:cinema_app/data/models/movie_result.dart';
 import 'package:cinema_app/data/models/movie_datails.dart';
 import 'package:cinema_app/data/utils/http_helper.dart';
 import 'package:cinema_app/domain/models/movie.dart';
@@ -8,7 +8,7 @@ import 'package:cinema_app/domain/models/movie.dart';
 class MovieApiClient {
   final _httpHelper = HttpHelper(MovieDbClient.dio);
 
-  /// Get now playing movies
+  /// Movies
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await _httpHelper.getList<MovieResult>(
       '/movie/now_playing',
