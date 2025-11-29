e2200f82265e7f7abeb3ef69f61eb8e879baa5c6
KevinMartinezC <ceronk003@gmail.com>
2025-11-28 19:11:27 -0600

    fix: serializarion error

--- lib/data/models/moviedb_response.dart
+++ lib/data/models/moviedb_response.dart
@@ -17,7 +17,7 @@ class MovieDbResponse {
 
   factory MovieDbResponse.fromJson(Map<String, dynamic> json) =>
       MovieDbResponse(
-        dates: json["dates"] ? Dates.fromJson(json["dates"]) : null,
+        dates: json["dates"] != null ? Dates.fromJson(json["dates"] as Map<String, dynamic>) : null,
         page: json["page"],
         results: List<MovieResult>.from(
           json["results"].map((x) => MovieResult.fromJson(x)),
